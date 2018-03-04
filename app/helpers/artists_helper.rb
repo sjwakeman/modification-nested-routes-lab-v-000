a8421c820d0bb148b94738df5098231ba53e23d0
Scott Wakeman <github email address>
2018-03-03 19:33:16 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
-    binding.pry
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
   def artist_select(ham)
-    binding.pry
-
+  
   end
 end
